5a858a8aa6adcbe35a778c35ad240c432cd98ce9
Maryem GHRIBI <mariemghribi0289@gmail.com>
2024-03-19 15:18:01 +0100

    🚀✨feature: initial chat-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import data from "./data.json";
+
+export enum MessageItemType {
+  REPLY = "reply",
+  USER = "user",
+}
+
+export interface MessageItem {
+  id: string;
+  type: MessageItemType;
+  text?: string | null;
+  responses: string[];
+}
+
+interface MessageProps {
+  message: MessageItem;
+}
+
+const UserMessage = ({ message }: MessageProps) => (
+  <div key={message.id} className="bg-white rounded-md mt-4">
+    <p className="text-dark-body px-2 py-1 text-sm m-0">{message.text}</p>
+  </div>
+);
+
+const ReplyMessage = ({ message }: MessageProps) => (
+  <div key={message.id} className="bg-secondary rounded-md mt-4">
+    <p className="text-dark-body px-2 py-1 text-sm m-0">{message.text}</p>
+  </div>
+);
 
 function App() {
+  const firstMessage = data.find((message) => message.root === true) as unknown as MessageItem | null;
+  const [currentMessage, setCurrentMessage] = useState<MessageItem | null>(firstMessage ?? null);
+  const [messageHistory, setMessageHistory] = useState<MessageItem[]>([]);
+
+  useEffect(() => {
+    if (firstMessage) {
+      setMessageHistory([firstMessage]);
+    }
+  }, [firstMessage]);
+
+  let showButtons = false;
+
+  if (currentMessage?.type === MessageItemType.USER) {
+    showButtons = true;
+  }
+
+  const showNextMessage = (currentMessage: MessageItem) => {
+    const nextMessage = data.find((message) => message.id === currentMessage.responses[0]);
+    if (nextMessage) {
+      setCurrentMessage(nextMessage as unknown as MessageItem);
+      setMessageHistory([...messageHistory, nextMessage as unknown as MessageItem]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container sm mx-auto">
+        <div className="flex flex-col h-screen">
+          <header className="h-16 bg-chat-header p-4">
+            <h2 className="text-dark-body text-lg">Jason</h2>
+          </header>
+          <main className="flex-grow overflow-auto flex-col-reverse bg-chat-body">
+            <div className="px-4 py-4">
+              {currentMessage && (
+                <>
+                  {currentMessage.type === MessageItemType.USER && (
+                    <UserMessage message={currentMessage} />
+                  )}
+                  {currentMessage.type === MessageItemType.REPLY && (
+                    <ReplyMessage message={currentMessage} />
+                  )}
+                </>
+              )}
+            </div>
+          </main>
+          <footer className="h-40 bg-chat-header">
+            <div className="px-2 py-4 flex flex-col gap-2">
+              <div>
+                {showButtons && (
+                  <button
+                    className="bg-white text-dark-body text-sm px-4 py-2 border-light-body border-2 border-solid rounded-xl"
+                    onClick={() => {
+                      console.log("Clicked");
+                      if (currentMessage) {
+                        showNextMessage(currentMessage);
+                      }
+                    }}
+                  >
+                    Response
+                  </button>
+                )}
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </>
   );
 }
 
